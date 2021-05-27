cdb6471298001cdc4f48be30ddb2aff377ab09be
渡辺孝之 <na8esin.mpst@gmail.com>
2021-05-27 20:55:55 +0900

    kotlinのプラグインがおかしくなって自動でimportを追加してくれないから手動で追加

--- app/src/main/java/com/example/wordsapp/WordAdapter.kt
+++ app/src/main/java/com/example/wordsapp/WordAdapter.kt
@@ -16,6 +16,8 @@
 package com.example.wordsapp
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
